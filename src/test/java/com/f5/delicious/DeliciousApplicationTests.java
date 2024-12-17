c1995272803bc22a87dfa81efa4a7cbedd9eff00
evymari <144184061+evymari@users.noreply.github.com>
2024-12-17 11:26:41 +0100

    feat: add develop

---
+++ src/test/java/com/f5/delicious/DeliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.delicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
